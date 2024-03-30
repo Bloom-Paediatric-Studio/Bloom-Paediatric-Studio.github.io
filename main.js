5dffbbcdf0683f2380b472f9e87ec520cb9783c6
djt97 <35709607+djt97@users.noreply.github.com>
2024-03-30 09:37:40 +0000

    Deploying to gh-pages from @ Bloom-Paediatric-Studio/Bloom-Paediatric-Studio.github.io@6f0bbb73653ad1ddbc5444099047adc1d3e795a5 🚀

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGp0aG9ybnRvbiIsImEiOiJjbHQ5eDhpM2gxOTRxMnFwdGNvYWQ0N2oyIn0.PC3PL79ac4eZg4pTWWraNg';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGp0aG9ybnRvbiIsImEiOiJjbHQ5eDhpM2gxOTRxMnFwdGNvYWQ0N2oyIn0.PC3PL79ac4eZg4pTWWraNg'; // Replace this with your actual Mapbox API key
+
+window.onload = function() {
+    var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/light-v11', // style URL
+        center: [150.65, -33.7385], // starting position [lng, lat]
+        zoom: 14.5, // starting zoom
+        accessToken: mapboxgl.accessToken // Provide the access token here
+    });
+
+    // Add a marker to the map
+    new mapboxgl.Marker()
+        .setLngLat([150.65, -33.7385])
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML('Location: Emu Heights NSW, Australia'))
+        .addTo(map);
+};
